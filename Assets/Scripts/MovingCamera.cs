27c2dbd7be0c229d9449150bfd6358a92d6feb20
Andrzej Zabski <jedrak32@gmail.com>
2020-01-28 09:53:45 +0100

    initial

---
+++ Assets/Scripts/MovingCamera.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(-pos.x * dragSpeed, -pos.y * dragSpeed);
+
+        transform.Translate(move, Space.World);
+
+        //Debug.Log(Input.mouseScrollDelta);
+        if(Input.GetAxis("Mouse ScrollWheel")> 0.0f)
+        {
+            transform.Translate(Vector3.forward);
+        }else if(Input.GetAxis("Mouse ScrollWheel") < 0.0f)
+        {
+            transform.Translate(Vector3.back);
+        }
+    }
+
+}
---
+++ Assets/Scripts/RoomGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class RoomGenerator : MonoBehaviour
+{
+    public List<GameObject> listOfTiles;
+    public InputField inW, inH;
+    public Slider slider;
+    public int[,] room;
+    public int width, height, emptyfields = 50, jump;
+
+
+
+    public void setW()
+    {
+        Int32.TryParse(inW.text, out width);
+    }
+
+    public void setH()
+    {
+        Int32.TryParse(inH.text, out height);
+    }
+
+    public void setEmpty()
+    {
+        emptyfields = (int) slider.value;
+        jump = (100 - emptyfields) / listOfTiles.Count;
+    }
+
+    public void generate()
+    {   
+        
+        room = new int[width, height];
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                
+                    int rand = Random.Range(0, 100), whichPrefab = 0;
+                    if (rand < emptyfields) whichPrefab = 0;
+                    else if (rand > emptyfields && rand < emptyfields + jump) whichPrefab = 1;
+                    else if (rand > emptyfields + jump && rand < emptyfields + 2*jump) whichPrefab = 2;
+                    else if (rand > emptyfields + 2*jump && rand < emptyfields + 3*jump) whichPrefab = 3;
+                    else if (rand > emptyfields + 3*jump && rand < emptyfields + 4*jump) whichPrefab = 4;
+                    else if (rand > emptyfields + 4*jump && rand < emptyfields + 5*jump) whichPrefab = 5;
+                    else if (rand > emptyfields + 5*jump && rand < emptyfields + 6*jump) whichPrefab = 6;
+                    else if (rand > emptyfields + 6*jump && rand < emptyfields + 7*jump) whichPrefab = 7;
+                    else if (rand > emptyfields + 7*jump && rand < emptyfields + 8*jump) whichPrefab = 8;
+                    else if (rand > emptyfields + 8*jump && rand < emptyfields + 9*jump) whichPrefab = 9;
+                    room[i, j] = whichPrefab;
+            }
+        }
+        instantiateRoom();
+    }
+
+
+    public void instantiateRoom()
+    {
+        foreach (Transform obj in GetComponentInChildren<Transform>())
+        {
+            if (!(obj.gameObject.tag == "Chest" || obj.gameObject.tag == "Spawner"))
+            {
+                Destroy(obj.gameObject);
+            }
+        }
+        for (int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                GameObject gameObject = Instantiate(listOfTiles[room[i, j]], new Vector3(i * 7.5f, j * 7.5f), Quaternion.Euler(0, 0, 0));
+                gameObject.transform.parent = transform;
+            }
+        }
+    }
+
+    void Start()
+    {
+        jump = 50 / listOfTiles.Count; 
+        //generateRoom();
+    }
+}
